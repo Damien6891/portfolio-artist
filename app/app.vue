128457aa1a3a9e731b519b61bf9d8eeedbd72df2
Damien <damien.h68@gmail.com>
2026-08-11 20:30:27 +0200

    fix : umami script

--- app/app.vue
+++ app/app.vue
@@ -11,13 +11,9 @@
 <script setup>
 useScrollReveal();
 
-useHead({
-  script: [
-    {
-      src: 'https://stats.damien-hantzer.com/script.js',
-      defer: true,
-      'data-website': 'ae9703d3-4ade-4acc-b1f3-c49b5152e84f',
-    },
-  ],
+useScript({
+  src: 'https://stats.damien-hantzer.com/script.js',
+  defer: true,
+  'data-website': 'ae9703d3-4ade-4acc-b1f3-c49b5152e84f',
 });
 </script>
